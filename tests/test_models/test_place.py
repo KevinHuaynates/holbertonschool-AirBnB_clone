82b4382615158edf63ccf621d3fcb0d534599377
KevinHuaynates <6992@holbertonstudents.com>
2023-12-17 20:25:28 -0800

    tests

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -4,6 +4,8 @@ from models.place import Place
 
 class TestPlace(unittest.TestCase):
     """Test cases for the Place class."""
+
     def test_create_instance(self):
+        """Test creating an instance of Place."""
         place = Place()
         self.assertIsInstance(place, Place)
