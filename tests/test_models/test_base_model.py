a2e4e46e97bbf3825f48462d37dc2309cb50a717
KevinHuaynates <6992@holbertonstudents.com>
2023-12-17 21:17:10 -0800

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,12 +4,17 @@ from models.base_model import BaseModel
 my_model = BaseModel()
 my_model.name = "My First Model"
 my_model.my_number = 89
+
+print("Before save():")
 print(my_model)
+
 my_model.save()
+
+print("After save():")
 print(my_model)
+
 my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-                                   my_model_json[key]))
+for key, value in my_model_json.items():
+    print("\t{}: ({}) - {}".format(key, type(value), value))
