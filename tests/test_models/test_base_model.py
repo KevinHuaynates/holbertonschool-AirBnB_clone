e6a29f0ecf63f49a47cdbb03200b79664ce978bf
KevinHuaynates <6992@holbertonstudents.com>
2023-12-17 21:12:55 -0800

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,15 @@
-import unittest
+#!/usr/bin/python3
 from models.base_model import BaseModel
 
-
-class TestBaseModel(unittest.TestCase):
-    """Test cases for the BaseModel class."""
-    def test_create_instance(self):
-        """Test creating an instance of BaseModel."""
-        model = BaseModel()
-        self.assertIsInstance(model, BaseModel)
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
+                                   my_model_json[key]))
