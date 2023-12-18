f950850759f209b8dbaf6b29638d7432d4888d1a
KevinHuaynates <6992@holbertonstudents.com>
2023-12-17 20:28:41 -0800

    models

---
+++ models/place.py
@@ -0,0 +1,7 @@
+from models.base_model import BaseModel
+
+class Place(BaseModel):
+    """Class Place that inherits from BaseModel."""
+    def __init__(self, *args, **kwargs):
+        """Initialize Place instance."""
+        super().__init__(*args, **kwargs)
