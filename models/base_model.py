9201ee1167d6420d795fc270e556878cf9504ffd
KevinHuaynates <6992@holbertonstudents.com>
2023-12-17 22:21:48 -0800

    tests

--- models/base_model.py
+++ models/base_model.py
@@ -10,32 +10,35 @@ class BaseModel:
     """BaseModel class"""
     def __init__(self, *args, **kwargs):
         """Initialization method"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-        else:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
+                    setattr(self, key, datetime.strptime(value,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
                     setattr(self, key, value)
-
-    def to_dict(self):
-        """Returns a dictionary"""
-        result = self.__dict__.copy()
-        result['__class__'] = self.__class__.__name__
-        result['created_at'] = self.created_at.isoformat()
-        result['updated_at'] = self.updated_at.isoformat()
-        return result
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        """Returns the string representation of the instance"""
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__
-        )
+        """Return string of BaseModel"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """Update the public instance attribute updated_at"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Return a dictionary containing __dict__"""
+        result_dict = self.__dict__.copy()
+        result_dict['__class__'] = self.__class__.__name__
+        result_dict['created_at'] = self.created_at.isoformat()
+        result_dict['updated_at'] = self.updated_at.isoformat()
+        return result_dict
 
 
-if __name__ == "__main__":
-    bm1 = BaseModel()
-    bm2 = BaseModel(**bm1.to_dict())
-    print(bm1 is not bm2)
+storage = FileStorage()
+storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,37 +1,43 @@
+#!/usr/bin/python3
+"""
+Module for FileStorage class
+"""
+
 import json
+import os
 from models.base_model import BaseModel
 
 
 class FileStorage:
-    """Class FileStorage to serialize and deserialize instances."""
+    """Serializes instances to JSON file"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """Return the dictionary __objects."""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
-        """Set in __objects the obj with key <obj class name>.id."""
+        """Set in __objects the obj with key """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)."""
-        serialized_objects = {}
-        for key, value in FileStorage.__objects.items():
-            serialized_objects[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
-            json.dump(serialized_objects, file)
+        """Serializes __objects to the JSON file"""
+        serialized_dict = {}
+        for key, value in self.__objects.items():
+            serialized_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(serialized_dict, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects."""
-        try:
-            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
-                loaded_objects = json.load(file)
-                for key, value in loaded_objects.items():
-                    class_name = key.split(".")[0]
-                    instance = BaseModel(**value)
-                    FileStorage.__objects[key] = instance
-        except FileNotFoundError:
-            pass
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split(".")
+                    obj = eval(class_name)(**value)
+                    self.__objects[key] = obj
+        else:
+            return
