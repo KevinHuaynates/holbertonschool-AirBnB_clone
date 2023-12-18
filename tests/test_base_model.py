307f80ce0c35774e52573650c75b044841a89a77
KevinHuaynates <6992@holbertonstudents.com>
2023-12-17 21:04:56 -0800

    test_base_model.py

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -2,14 +2,10 @@
 from models.base_model import BaseModel
 
 my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
 my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]),
-          my_model_json[key]))
+
+output_after_save = str(my_model)
+expected_output_after_save = "[BaseModel] ({}) {}".format(my_model.id,
+                                                          my_model.__dict__)
+assert output_after_save == expected_output_after_save,
+"Incorrect output : Test BaseModel: save()"
