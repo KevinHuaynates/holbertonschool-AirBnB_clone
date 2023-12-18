90071d4f9fdaaf659acb214b4410d0167cfaecd9
KevinHuaynates <6992@holbertonstudents.com>
2023-12-17 21:57:34 -0800

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,41 @@
+#!/usr/bin/python3
+"""
+BaseModel module
+"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Base model for AirBnB project."""
+    """BaseModel class"""
     def __init__(self, *args, **kwargs):
-        """Initialize base model instance."""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value,
-                            "%Y-%m-%dT%H:%M:%S.%f"))
-                else:
-                    setattr(self, key, value)
-        else:
+        """Initialization method"""
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-
-    def __str__(self):
-        """Return string of BaseModel."""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
-
-    def save(self):
-        """Update the public instance attribute updated_at"""
-        self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
 
     def to_dict(self):
-        """Return a dictionary containing all keys/values of __dict__ """
-        result_dict = self.__dict__.copy()
-        result_dict['__class__'] = self.__class__.__name__
-        result_dict['created_at'] = self.created_at.isoformat()
-        result_dict['updated_at'] = self.updated_at.isoformat()
-        return result_dict
+        """Returns a dictionary"""
+        result = self.__dict__.copy()
+        result['__class__'] = self.__class__.__name__
+        result['created_at'] = self.created_at.isoformat()
+        result['updated_at'] = self.updated_at.isoformat()
+        return result
+
+    def __str__(self):
+        """Returns the string representation of the instance"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
+
+
+if __name__ == "__main__":
+    bm1 = BaseModel()
+    bm2 = BaseModel(**bm1.to_dict())
+    print(bm1 is not bm2)
