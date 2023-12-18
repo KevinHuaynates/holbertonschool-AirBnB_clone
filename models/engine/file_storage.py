55bdb5a21d480e705f09c5560179ffe77ab64732
KevinHuaynates <6992@holbertonstudents.com>
2023-12-18 00:36:54 -0800

    models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,25 +12,28 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary __objects"""
-        return self.__objects 
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (path: __file_path)"""
-        serialized = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w', encoding="utf-8") as file:
-            json.dump(serialized, file)
+        serialized_objects = {}
+        for key, obj in FileStorage.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(serialized_objects, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r', encoding="utf-8") as file:
-                loaded = json.load(file)
-                self.__objects = {key: models.classes[class_name](**value)
-                                  for key, value in loaded.items()}
+            from models import classes
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as file:
+                serialized_objects = json.load(file)
+                self.__objects = {key: classes[class_name](**value)
+                                  for key, (class_name, value) in serialized_objects.items()}
         except FileNotFoundError:
             pass
