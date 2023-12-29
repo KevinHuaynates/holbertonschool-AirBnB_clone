07da877cf0fddc325beb534e1be410e8aed6d575
KevinHuaynates <6992@holbertonstudents.com>
2023-12-29 00:01:09 -0800

    models

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module to create unique instance of FileStorage"""
-
-from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-
-classes = {"BaseModel": BaseModel}
-storage = FileStorage()
-storage.reload()
