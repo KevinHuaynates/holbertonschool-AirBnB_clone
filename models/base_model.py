f3df46428aecf09e3185642d42823519e0a891ef
KevinHuaynates <6992@holbertonstudents.com>
2023-12-29 01:33:56 -0800

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ This module contains the BaseModel class for the AirBnB project.
 
 import uuid
 from datetime import datetime
-import models
+from models import storage
 from .engine.file_storage import FileStorage
 
 class BaseModel:
@@ -30,7 +30,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.FileStorage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """
@@ -43,7 +43,7 @@ class BaseModel:
         Updates the attribute updated_at with the current datetime.
         """
         self.updated_at = datetime.now()
-        models.FileStorage.new(self)
+        storage.new(self)
 
     def to_dict(self):
         """
