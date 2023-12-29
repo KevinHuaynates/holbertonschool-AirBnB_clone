56e64d859d6f00806a13d3ba3c5a5511901237f6
KevinHuaynates <6992@holbertonstudents.com>
2023-12-28 22:16:14 -0800

    console.py

--- console.py
+++ console.py
@@ -76,7 +76,7 @@ class HBNBCommand(cmd.Cmd):
             elif len(args) < 2:
                 print("** instance id missing **")
             else:
-                instance_id = args[1].rstrip('\"')
+                instance_id = ' '.join(args[1:])
                 key = "{}.{}".format(class_name, instance_id)
                 if key in storage.all():
                     print(storage.all()[key])
