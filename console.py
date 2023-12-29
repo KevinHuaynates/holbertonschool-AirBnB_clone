ad857af4f409126a66519f99bcd8de6461520853
KevinHuaynates <6992@holbertonstudents.com>
2023-12-28 21:55:50 -0800

    console.py

--- console.py
+++ console.py
@@ -1,20 +1,45 @@
 #!/usr/bin/python3
+"""
+This module contains the entry point of the command interpreter.
+"""
+
 import cmd
 
-
 class HBNBCommand(cmd.Cmd):
-    """Command interpreter for AirBnB project."""
+"""This class defines the command interpreter"""
+
     prompt = '(hbnb) '
 
+    def emptyline(self):
+    """Do nothing on an empty line + ENTER.
+        """
+        pass
+
     def do_quit(self, arg):
-        """Quit command to exit the console."""
+        """
+        Quit command to exit the program.
+        """
         return True
 
+    def help_quit(self):
+        """
+        Help message for the quit command.
+        """
+        print("Quit command to exit the program")
+
     def do_EOF(self, arg):
-        """Exit on EOF."""
+        """
+        Handles the EOF signal to exit the program.
+        """
         print()
         return True
 
+    def help_EOF(self):
+        """
+        Help message for the EOF signal.
+        """
+        print("EOF signal to exit the program")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
