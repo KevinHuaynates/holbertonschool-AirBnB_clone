a5a4b1df205ac088b8b875617b79c0ada084653c
KevinHuaynates <6992@holbertonstudents.com>
2023-12-29 03:02:52 -0800

    models

---
+++ models/main.py
@@ -0,0 +1,9 @@
+# main.py
+
+from models import initialize_storage
+
+# Llamamos a initialize_storage antes de realizar operaciones que necesitan FileStorage
+initialize_storage()
+
+# Resto del código de tu aplicación
+
