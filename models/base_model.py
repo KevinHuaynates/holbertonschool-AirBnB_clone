8b67a9ff14aafc788141ad8d4245d75d15426fc8
KevinHuaynates <6992@holbertonstudents.com>
2023-12-30 04:37:08 -0800

    6

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        """Instantiates a new model"""
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != "__class__":
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, value)
             if "id" not in kwargs:
-                setattr(self, "id", str(uuid.uuid4()))
-            time_now = datetime.utcnow()
+                setattr(self, 'id', str(uuid.uuid4()))
+            time_format = "%Y-%m-%dT%H:%M:%S.%f"
             if "created_at" not in kwargs:
-                setattr(self, "created_at", time_now)
+                setattr(self, 'created_at', datetime.now())
             if "updated_at" not in kwargs:
-                setattr(self, "updated_at", time_now)
+                setattr(self, 'updated_at', datetime.now())
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
-            storage.new(self)
+            self.created_at = self.updated_at = datetime.now()
 
     def save(self):
-        self.updated_at = datetime.utcnow()
-        storage.save()
+        """Updates the updated_at attribute with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        dict_copy = self.__dict__.copy()
-        dict_copy['__class__'] = self.__class__.__name__
-        dict_copy['created_at'] = self.created_at.isoformat()
-        dict_copy['updated_at'] = self.updated_at.isoformat()
-        return dict_copy
+        """Returns a dictionary containing all keys/values of __dict__"""
+        result = dict(self.__dict__)
+        result['__class__'] = self.__class__.__name__
+        result['created_at'] = self.created_at.isoformat()
+        result['updated_at'] = self.updated_at.isoformat()
+        return result
 
-    def reload(self):
-        storage.reload()
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, str(self.__dict__))
 
