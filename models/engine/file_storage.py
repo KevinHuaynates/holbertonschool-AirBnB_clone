51e3eb30a9950af1b0ad80035f45654627570497
KevinHuaynates <6992@holbertonstudents.com>
2023-12-30 03:42:38 -0800

    6

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,10 +2,11 @@
 """
 File Storage Module
 
-This module defines the FileStorage class.
+This module defines the FileStorage class for serializing and deserializing instances to/from a JSON file.
 """
 
 import json
+from models.base_model import BaseModel
 
 class FileStorage:
     """FileStorage class."""
@@ -14,7 +15,29 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary __objects."""
-        return FileStorage.__objects
+        return self.__objects
 
-    # Otros métodos y propiedades de la clase FileStorage según sea necesario
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        with open(self.__file_path, mode='w', encoding='utf-8') as file:
+            obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists)."""
+        try:
+            with open(self.__file_path, mode='r', encoding='utf-8') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    cls_name, obj_id = key.split(".")
+                    cls = BaseModel if cls_name == "BaseModel" else None  # Add other class names as needed
+                    if cls:
+                        self.__objects[key] = cls(**value)
+        except FileNotFoundError:
+            pass
 
