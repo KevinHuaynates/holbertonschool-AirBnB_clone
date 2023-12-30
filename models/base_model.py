ff90ba7134ea35279c0cde91b18acc627d9c6f3e
KevinHuaynates <6992@holbertonstudents.com>
2023-12-30 02:50:24 -0800

    3

--- models/base_model.py
+++ models/base_model.py
@@ -12,9 +12,32 @@ class BaseModel:
     """BaseModel class."""
     def __init__(self, *args, **kwargs):
         """Initialization method."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """String representation of the object."""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime."""
         self.updated_at = datetime.now()
 
-    # Agrega aquí otros métodos y propiedades de la clase BaseModel según sea necesario
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
 
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,11 +1,82 @@
 # tests/test_models/test_base_model.py
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 class TestBaseModel(unittest.TestCase):
     def test_create_instance(self):
         obj = BaseModel()
         self.assertIsInstance(obj, BaseModel)
 
-    # Agrega más pruebas según sea necesario
+    def test_str_method(self):
+        obj = BaseModel()
+        expected_str = "[BaseModel] ({}) {}".format(obj.id, obj.__dict__)
+        self.assertEqual(str(obj), expected_str)
+
+    def test_save_method(self):
+        obj = BaseModel()
+        old_updated_at = obj.updated_at
+        obj.save()
+        new_updated_at = obj.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+
+    def test_to_dict_method(self):
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertEqual(obj_dict['id'], obj.id)
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertEqual(
+            obj_dict['created_at'], obj.created_at.isoformat()
+        )
+        self.assertEqual(
+            obj_dict['updated_at'], obj.updated_at.isoformat()
+        )
+
+    def test_to_dict_with_additional_attribute(self):
+        obj = BaseModel()
+        obj.name = "Test"
+        obj_dict = obj.to_dict()
+        self.assertEqual(obj_dict['name'], "Test")
+
+    def test_to_dict_with_datetime_objects(self):
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        self.assertIsInstance(obj_dict['created_at'], str)
+        self.assertIsInstance(obj_dict['updated_at'], str)
+
+    def test_to_dict_with_non_datetime_objects(self):
+        obj = BaseModel()
+        obj.some_data = [1, 2, 3]
+        obj_dict = obj.to_dict()
+        self.assertEqual(obj_dict['some_data'], [1, 2, 3])
+
+    def test_create_instance_with_dict(self):
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        new_obj = BaseModel(**obj_dict)
+        self.assertIsInstance(new_obj, BaseModel)
+        self.assertEqual(new_obj.id, obj.id)
+        self.assertEqual(new_obj.created_at, obj.created_at)
+        self.assertEqual(new_obj.updated_at, obj.updated_at)
+
+    def test_create_instance_with_dict_additional_attribute(self):
+        obj = BaseModel()
+        obj.name = "Test"
+        obj_dict = obj.to_dict()
+        new_obj = BaseModel(**obj_dict)
+        self.assertEqual(new_obj.name, "Test")
+
+    def test_create_instance_with_dict_datetime_objects(self):
+        obj = BaseModel()
+        obj_dict = obj.to_dict()
+        new_obj = BaseModel(**obj_dict)
+        self.assertIsInstance(new_obj.created_at, datetime)
+        self.assertIsInstance(new_obj.updated_at, datetime)
+
+    def test_create_instance_with_dict_non_datetime_objects(self):
+        obj = BaseModel()
+        obj.some_data = [1, 2, 3]
+        obj_dict = obj.to_dict()
+        new_obj = BaseModel(**obj_dict)
+        self.assertEqual(new_obj.some_data, [1, 2, 3])
 
