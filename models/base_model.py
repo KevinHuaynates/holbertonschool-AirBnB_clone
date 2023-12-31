7869d0193b5f0cdb76edec400934a04ee21a961c
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 06:47:13 -0800

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,34 @@
-# models/base_model.py
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+
+        # Cambio aquí para evitar la importación circular
+        models.storage.new(self)
 
     def to_dict(self):
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        return obj_dict
+        new_dict = self.__dict__.copy()
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return new_dict
 
     def save(self):
         self.updated_at = datetime.now()
-        storage.save()
-from models import storage
+        models.storage.save()
+
+# Cambio aquí para evitar la importación circular
+storage = None
+
