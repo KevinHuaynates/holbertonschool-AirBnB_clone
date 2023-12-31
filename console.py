6042e5760a00513d2c96a6231b81c54e18b24d9b
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 07:26:23 -0800

    console.py

--- console.py
+++ console.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""
-AirBnB Clone Console Module
-
-This module provides an interactive console for managing AirBnB objects.
-"""
+"""This module defines the entry point of the command interpreter"""
 
 import cmd
 
-class HBNBCommand(cmd.Cmd):
-    """Command interpreter class."""
-    prompt = '(hbnb) '
 
-    def do_help(self, arg):
-        """Prints a list of available commands."""
-        print("Available commands: help, quit")
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class"""
+
+    prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Exits the console."""
+        """Quit command to exit the program"""
         return True
 
-    # Agrega aquí la implementación de otros comandos según tus necesidades
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print("")
+        return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
