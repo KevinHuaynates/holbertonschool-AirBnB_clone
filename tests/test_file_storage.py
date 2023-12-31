938c7b1e00f34e10ccb24bc5ba4b1e2c312a4f31
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 07:07:53 -0800

    3

--- tests/test_file_storage.py
+++ tests/test_file_storage.py
@@ -2,19 +2,27 @@
 
 import unittest
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 class TestFileStorage(unittest.TestCase):
     def test_file_path(self):
-        # Your test code here
+        storage = FileStorage()
+        self.assertEqual(storage._FileStorage__file_path, "file.json")
 
     def test_objects(self):
-        # Your test code here
+        storage = FileStorage()
+        self.assertEqual(storage._FileStorage__objects, {})
 
     def test_all(self):
-        # Your test code here
+        storage = FileStorage()
+        self.assertEqual(storage.all(), {})
 
     def test_new(self):
-        # Your test code here
+        storage = FileStorage()
+        obj = BaseModel()
+        storage.new(obj)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.assertIn(key, storage._FileStorage__objects)
 
     def test_save(self):
         # Your test code here
