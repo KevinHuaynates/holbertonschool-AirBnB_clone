9d7e264bd27f1a2ae5b8ae9431e99ff803b8630f
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 06:42:30 -0800

    tests

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,36 +1,42 @@
-# models/engine/file_storage.py
+"""File Storage Module"""
+
 import json
+from models.base_model import BaseModel
 
 class FileStorage:
+    """Serializes instances to a JSON file and deserializes JSON file to instances"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        serialized_objects = {}
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = {}
         for key, obj in self.__objects.items():
-            serialized_objects[key] = obj.to_dict()
+            obj_dict[key] = obj.to_dict()
 
-        with open(self.__file_path, 'w') as file:
-            json.dump(serialized_objects, file)
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(obj_dict, file)
 
     def reload(self):
+        """Deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as file:
-                loaded_objects = json.load(file)
-
-            for key, obj_dict in loaded_objects.items():
-                class_name, obj_id = key.split('.')
-                class_ = eval(class_name)
-                obj_instance = class_(**obj_dict)
-                self.__objects[key] = obj_instance
-
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name = value['__class__']
+                    class_ = globals()[class_name]  # Access the class globally
+                    obj_instance = class_(**value)
+                    self.__objects[key] = obj_instance
         except FileNotFoundError:
-            pass
+            pass  # No exception should be raised if the file doesn't exist
 
