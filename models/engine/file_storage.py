a57774ddd469d901200f90be51146c00049db90b
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 06:59:20 -0800

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,34 +4,39 @@ import json
 from models.base_model import BaseModel
 
 class FileStorage:
+    """Serializes instances to a JSON file and deserializes JSON file to instances"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        objects_dict = {}
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = {}
         for key, obj in self.__objects.items():
-            objects_dict[key] = obj.to_dict()
+            obj_dict[key] = obj.to_dict()
 
         with open(self.__file_path, 'w', encoding='utf-8') as file:
-            json.dump(objects_dict, file)
+            json.dump(obj_dict, file)
 
     def reload(self):
+        """Deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as file:
-                objects_dict = json.load(file)
-
-            for key, obj_dict in objects_dict.items():
-                class_name, obj_id = key.split('.')
-                obj = eval(class_name)(**obj_dict)
-                self.__objects[key] = obj
-
+                obj_dict = json.load(file)
+                for key, value in obj_dict.items():
+                    class_name = value['__class__']
+                    class_ = globals()[class_name]  # Access the class globally
+                    obj_instance = class_(**value)
+                    self.__objects[key] = obj_instance
         except FileNotFoundError:
-            pass  # No hagas nada si el archivo no existe
+            pass  # No exception should be raised if the file doesn't exist
 
