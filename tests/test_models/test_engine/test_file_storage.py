2e7b49e02a11ab951ea56410195285ce7cc126bf
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 07:12:50 -0800

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,11 +1,41 @@
-# tests/test_engine/test_file_storage.py
+# tests/test_file_storage.py
+
 import unittest
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+import os
 
 class TestFileStorage(unittest.TestCase):
-    def test_create_instance(self):
-        file_storage = FileStorage()
-        self.assertIsInstance(file_storage, FileStorage)
+    def setUp(self):
+        self.storage = FileStorage()
 
-    # Agrega más pruebas según sea necesario
+    def tearDown(self):
+        if os.path.exists(self.storage._FileStorage__file_path):
+            os.remove(self.storage._FileStorage__file_path)
+
+    def test_file_path(self):
+        self.assertEqual(self.storage._FileStorage__file_path, "file.json")
+
+    def test_objects(self):
+        self.assertEqual(self.storage._FileStorage__objects, {})
+
+    def test_all(self):
+        self.assertEqual(self.storage.all(), {})
+
+    def test_new(self):
+        obj = BaseModel()
+        self.storage.new(obj)
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.assertIn(key, self.storage._FileStorage__objects)
+
+    def test_save_reload(self):
+        obj = BaseModel()
+        self.storage.new(obj)
+        self.storage.save()
+        self.storage.reload()
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.assertIn(key, self.storage._FileStorage__objects)
+
+if __name__ == "__main__":
+    unittest.main()
 
