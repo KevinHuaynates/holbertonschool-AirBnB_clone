c94f0815747cb3e843b5a65af9757ff176c0c04f
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 06:52:24 -0800

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,40 +1,34 @@
 import json
-from models.base_model import BaseModel
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances"""
-
-    __file_path = "file.json"
+    __file_path = "file.json"  # Asegúrate de que el nombre del archivo sea el correcto
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects"""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
-        obj_dict = {}
+        objects_dict = {}
         for key, obj in self.__objects.items():
-            obj_dict[key] = obj.to_dict()
+            objects_dict[key] = obj.to_dict()
 
         with open(self.__file_path, 'w', encoding='utf-8') as file:
-            json.dump(obj_dict, file)
+            json.dump(objects_dict, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, 'r', encoding='utf-8') as file:
-                obj_dict = json.load(file)
-                for key, value in obj_dict.items():
-                    class_name = value['__class__']
-                    class_ = globals()[class_name]  # Access the class globally
-                    obj_instance = class_(**value)
-                    self.__objects[key] = obj_instance
-        except Exception as e:
-            print(f"Error reloading data: {e}")
+                objects_dict = json.load(file)
+
+            for key, obj_dict in objects_dict.items():
+                class_name, obj_id = key.split('.')
+                obj = eval(class_name)(**obj_dict)
+                self.__objects[key] = obj
+
+        except FileNotFoundError:
+            pass  # No hagas nada si el archivo no existe
 
