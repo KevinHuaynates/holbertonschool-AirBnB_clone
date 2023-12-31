a6c4185710b827343816edd48339987a1da709cc
KevinHuaynates <6992@holbertonstudents.com>
2023-12-31 07:05:32 -0800

    tests

---
+++ tests/test_file_storage.py
@@ -0,0 +1,27 @@
+# tests/test_file_storage.py
+
+import unittest
+from models.engine.file_storage import FileStorage
+
+class TestFileStorage(unittest.TestCase):
+    def test_file_path(self):
+        # Your test code here
+
+    def test_objects(self):
+        # Your test code here
+
+    def test_all(self):
+        # Your test code here
+
+    def test_new(self):
+        # Your test code here
+
+    def test_save(self):
+        # Your test code here
+
+    def test_reload(self):
+        # Your test code here
+
+if __name__ == "__main__":
+    unittest.main()
+
